f18cc3dbc08f64c29dd739235849cd71d9563e65
David Marte <redesigndavid@gmail.com>
2025-05-20 12:27:41 +0800

    Adding post page

--- frontend/jdblog/src/main.jsx
+++ frontend/jdblog/src/main.jsx
@@ -6,6 +6,7 @@ import App from "./App.jsx";
 import Layout from "./components/Layout";
 import BlogLayout from "./components/BlogLayout";
 import Test from "./Test";
+import Post from "./components/Post";
 import ThemeProvider from "./context/ThemeProvider";
 
 createRoot(document.getElementById("root")).render(
@@ -19,6 +20,7 @@ createRoot(document.getElementById("root")).render(
           <Route path="/" element={<Layout sidebar={true}/>}>
             <Route element={<BlogLayout />}>
               <Route path="test" element={<Test />} />
+              <Route path="post/:postid" element={<Post />} />
             </Route>
           </Route>
         </Routes>
