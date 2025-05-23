f3167e7ed5b07bdfc1932bbc9e30f1f2b56d82bb
David Marte <redesigndavid@gmail.com>
2025-05-24 00:15:51 +0800

    Working post edit

--- frontend/jdblog/src/components/MD.jsx
+++ frontend/jdblog/src/components/MD.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import Highlight from "react-highlight";
+import rehypeRaw from "rehype-raw";
 import { AA } from "../Tracker";
 
 const FlattenPre = ({ children }) => {
@@ -16,10 +17,11 @@ const FlattenPre = ({ children }) => {
 function MD({ children }) {
   return (
     <>
-      <div className="xl:w-3xl w-5xl bb max-w-dvw leading-8 prose prose-xl prose-pre:font-[Victor_Mono] prose-pre:text-md font-normal dark:prose-invert prose-a:text-emerald-500 xl:px-0 px-4">
+      <div className="leading-8 prose prose-xl prose-pre:font-[Victor_Mono] prose-pre:text-md font-normal dark:prose-invert prose-a:text-emerald-500 xl:px-0 px-4">
         <Markdown
           components={{ code: Highlight, pre: FlattenPre, a: AA }}
           remarkPlugins={[remarkGfm]}
+          rehypePlugins={[rehypeRaw]}
         >
           {children}
         </Markdown>
